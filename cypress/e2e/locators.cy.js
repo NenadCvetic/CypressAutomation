0643aff59892f873b820a3ed0d7b53e0885ebe23
NenadCvetic <cveticnenad92@gmail.com>
2023-10-30 10:16:44 +0100

    web elements in list

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -17,8 +17,20 @@ it(('Check different locators strategies'), () => {
     cy.get("input[name='username']").type('CydeoStudent'); //every statement creates an object with which you can interact with
     //and next command makes operation to the object created at the previous statement
 
+    cy.get("[type='text']").clear(); //clear what is typed in the field
 
+    cy.get("input").each((item, index, list) => {
 
+        //assert that lenght of list is 2
+        expect(list).to.have.length(2);
+
+        expect(item).to.have.attr("type");
+
+    
+
+        
+
+    })
 
 
 })
